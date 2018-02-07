902d8e89c2888f04a6413b1aec98aecba55bd11f
neroZWX <971370106@qq.com>
2018-02-07 21:15:30 +0000

    bacis animation

---
+++ Assets/PlayerAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+    private CharacterController characterController;
+    private Animation anim;
+	// Use this for initialization
+	void Start () {
+        characterController = this.GetComponent<CharacterController>();
+        anim = GetComponent<Animation>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterController.isGrounded == false)
+        {
+           anim.CrossFade("soldierFalling");
+        }
+        else {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            if (Mathf.Abs(h) > 0.03f || Mathf.Abs(v) > 0.03f)
+            {
+                anim.CrossFade("soldierWalk");
+            }
+            else {
+                anim.CrossFade("soldierIdle");
+            }
+        }
+         
+    }
+	}
+
