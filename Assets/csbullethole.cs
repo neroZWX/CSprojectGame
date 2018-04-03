fa7d2131b6bd2817bce4e8185c9992561bc15d36
neroZWX <971370106@qq.com>
2018-04-03 23:18:32 +0100

    bulletEffect

---
+++ Assets/csbullethole.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csbullethole : MonoBehaviour {
+    public float speed = 0.3f;
+    private float timer = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer > 2) {
+            GetComponent<Renderer>().material.color = Color.Lerp(GetComponent<Renderer>().material.color, Color.clear, speed * Time.deltaTime);
+        }
+        if (timer > 10) {
+            Destroy(this.gameObject);
+        }
+	}
+}
